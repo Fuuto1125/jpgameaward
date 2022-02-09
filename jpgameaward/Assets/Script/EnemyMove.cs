7fac3fcb5a37289d85f04ef3ed0ee7bd93750395
KBC12006023\kbc12006023 <ryusei-miyazato-20h@stu.kbc.ac.jp>
2022-02-09 09:22:41 +0900

    敵（火の玉）制作中…

---
+++ jpgameaward/Assets/Script/EnemyMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyMove : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("追いかける対象")]
+    private GameObject player;
+
+    private NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        // NavMeshAgentを保持しておく
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        // プレイヤーを目指して進む
+        navMeshAgent.destination = player.transform.position;
+    }
+}
