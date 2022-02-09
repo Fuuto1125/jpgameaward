6799f936c4bef930a659ae6a65289e5aa6392ca8
KBC12006149\futon <futo-yamauti-20h@stu.kbc.ac.jp>
2022-02-09 14:50:50 +0900

    動くけど変な動きをする

---
+++ jpgameaward/Assets/Playermove.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermove : MonoBehaviour
+{
+    [SerializeField] int Speed, jump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//キーボードの上下キーでの操作
+		if (Input.GetKey("w"))
+		{
+			transform.position += transform.forward * Speed * Time.deltaTime;
+		}
+		if (Input.GetKey("s"))
+		{
+			transform.position -= transform.forward * Speed * Time.deltaTime;
+		}
+		if (Input.GetKey("d"))
+		{
+			transform.position -= transform.right * Speed * Time.deltaTime;
+		}
+		if (Input.GetKey("a"))
+		{
+			transform.position -= transform.right * Speed * Time.deltaTime;
+		}
+		//コントローラーでの操作
+		if (Input.GetAxis("Vertical") > 0)
+		{
+			transform.position -= transform.forward * Speed * Time.deltaTime;
+		}
+		if (Input.GetAxis("Vertical") < 0)
+		{
+			transform.position += transform.forward * Speed * Time.deltaTime;
+		}
+		if (Input.GetAxis("Horizontal") > 0)
+		{
+			transform.position -= transform.right * Speed * Time.deltaTime;
+		}
+		if (Input.GetAxis("Horizontal") < 0)
+		{
+			transform.position += transform.right * Speed * Time.deltaTime;
+		}
+	}
+}
+
